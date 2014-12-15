2a2e29fa67ba358227eec2dd7980acf87e717d51
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2014-12-15 14:07:13 +0200

    Load missing sdks classes in account and oauth helpers

--- app/code/community/Nosto/Tagging/Helper/Oauth.php
+++ app/code/community/Nosto/Tagging/Helper/Oauth.php
@@ -24,6 +24,8 @@
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
+require_once(Mage::getBaseDir('lib').'/nosto/php-sdk/src/config.inc.php');
+
 /**
  * Helper class for OAuth2 related tasks.
  *
