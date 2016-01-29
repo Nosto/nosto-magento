4fbd19c884d0495ad723d4fa856078c0c72d1de6
Hannu Pölönen <hannu.polonen@nosto.com>
2016-01-29 10:24:43 +0200

    Update copyright

--- app/code/community/Nosto/Tagging/Block/Adminhtml/System/Config/Ajax/Button.php
+++ app/code/community/Nosto/Tagging/Block/Adminhtml/System/Config/Ajax/Button.php
@@ -21,7 +21,7 @@
  * @category  design
  * @package   adminhtml_default_default
  * @author    Nosto Solutions Ltd <magento@nosto.com>
- * @copyright Copyright (c) 2013-2015 Nosto Solutions Ltd (http://www.nosto.com)
+ * @copyright Copyright (c) 2013-2016 Nosto Solutions Ltd (http://www.nosto.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
