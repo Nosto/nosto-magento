61c34d2ded613bcb22772bbd3dd369c777f4808e
Hannu Pölönen <hannu.polonen@nosto.com>
2016-04-22 10:45:02 +0300

    Add search term tagging

---
+++ app/code/community/Nosto/Tagging/Block/Searchterm.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Magento
+ *  
+ * NOTICE OF LICENSE
+ *  
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *  
+ * DISCLAIMER
+ *  
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *  
+ * @category  Nosto
+ * @package   Nosto_Tagging
+ * @author    Nosto Solutions Ltd <magento@nosto.com>
+ * @copyright Copyright (c) 2013-2016 Nosto Solutions Ltd (http://www.nosto.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Search term tagging block.
+ * Adds search term tag to the HTML document.
+ *
+ * @category Nosto
+ * @package  Nosto_Tagging
+ * @author   Nosto Solutions Ltd <magento@nosto.com>
+ */
+class Nosto_Tagging_Block_Searchterm extends Mage_Core_Block_Template
+{
+    /**
+     * Render shopping search term content
+     *
+     * @return string
+     */
+    protected function _toHtml()
+    {
+        if (!Mage::helper('nosto_tagging')->isModuleEnabled()
+            || !Mage::helper('nosto_tagging/account')->existsAndIsConnected()
+        ) {
+            return '';
+        }
+
+        return parent::_toHtml();
+    }
+
+    /**
+     * Returns the search term
+     *
+     * @return string
+     */
+    public function getSearchTerm()
+    {
+        return $this->helper('catalogsearch')->getQueryText();
+    }
+}
