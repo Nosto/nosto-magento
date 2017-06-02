f9090f8657176ba94f3560b3ba6d150a7e0641d0
Mridang Agarwalla <mridang@nosto.com>
2017-06-02 10:42:10 +0300

    Added support to conditionally render the stub if account exists.

---
+++ app/code/community/Nosto/Tagging/Block/Stub.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Magento
+ *  
+ * NOTICE OF LICENSE
+ *  
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *  
+ * DISCLAIMER
+ *  
+ * Do not edit or add to this file if you wish to upgrade Magento to newer
+ * versions in the future. If you wish to customize Magento for your
+ * needs please refer to http://www.magentocommerce.com for more information.
+ *  
+ * @category  Nosto
+ * @package   Nosto_Tagging
+ * @author    Nosto Solutions Ltd <magento@nosto.com>
+ * @copyright Copyright (c) 2013-2017 Nosto Solutions Ltd (http://www.nosto.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Nosto JS stub block
+ *
+ * @category Nosto
+ * @package  Nosto_Tagging
+ * @author   Nosto Solutions Ltd <magento@nosto.com>
+ */
+class Nosto_Tagging_Block_Stub extends Mage_Core_Block_Template
+{
+    /**
+     * Render HTML for the page type if the module is enabled for the current
+     * store.
+     *
+     * @return string
+     */
+    protected function _toHtml()
+    {
+        /** @var Nosto_Tagging_Helper_Account $helper */
+        $helper = Mage::helper('nosto_tagging/account');
+        if (!Mage::helper('nosto_tagging')->isModuleEnabled() || !$helper->existsAndIsConnected()) {
+            return '';
+        }
+
+        return parent::_toHtml();
+    }
+}
