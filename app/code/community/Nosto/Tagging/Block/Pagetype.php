33d5ff80283eef7dde61d8705eb93c4eef56c8f6
Cid Lopes <alannettto@gmail.com>
2018-06-01 16:08:58 +0300

    Removes PageType Template

--- app/code/community/Nosto/Tagging/Block/Pagetype.php
+++ app/code/community/Nosto/Tagging/Block/Pagetype.php
@@ -50,16 +50,7 @@ class Nosto_Tagging_Block_Pagetype extends Mage_Core_Block_Template
      */
     protected function _toHtml()
     {
-        /** @var Nosto_Tagging_Helper_Account $helper */
-        $helper = Mage::helper('nosto_tagging/account');
-        if (
-            !Mage::helper('nosto_tagging/module')->isModuleEnabled()
-            || !$helper->existsAndIsConnected()
-        ) {
-            return '';
-        }
-
-        return parent::_toHtml();
+        return (new Nosto_Object_PageType($this->getPageTypeName()))->toHtml();
     }
 
     /**
