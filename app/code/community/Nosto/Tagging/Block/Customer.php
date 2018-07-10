d283d0ba4f99f1a58b02f9d9cc323250951c0150
Cid Lopes <alannettto@gmail.com>
2018-07-10 11:48:04 +0300

    Fix Review Fidings & Fix Customer Block Inspections

--- app/code/community/Nosto/Tagging/Block/Customer.php
+++ app/code/community/Nosto/Tagging/Block/Customer.php
@@ -42,7 +42,6 @@ class Nosto_Tagging_Block_Customer extends Mage_Customer_Block_Account_Dashboard
      * the module is enabled for the current store.
      *
      * @return string
-     * @throws Varien_Exception
      */
     protected function _toHtml()
     {
@@ -80,22 +79,25 @@ class Nosto_Tagging_Block_Customer extends Mage_Customer_Block_Account_Dashboard
      */
     public function getNostoCustomer()
     {
+        /* @var Nosto_Tagging_Helper_Data $helper */
         $helper = Mage::helper('nosto_tagging');
         $store = Mage::app()->getStore();
-        /* @var Nosto_Tagging_Helper_Data $helper */
         $customer = $this->getCustomer();
-        $email = $customer->getEmail();
-        if (!$this->getCustomer() instanceof Mage_Customer_Model_Customer
-            || $email === null
+        /** @noinspection PhpUndefinedMethodInspection */
+        if (!$customer instanceof Mage_Customer_Model_Customer
+            || $customer->getEmail() === null
             || !$helper->getSendCustomerData($store)
         ) {
             return null;
         }
-
         /** @var Nosto_Tagging_Helper_Email $emailHelper */
         $emailHelper = Mage::helper('nosto_tagging/email');
+        /** @noinspection PhpUndefinedMethodInspection */
+        $email = $customer->getEmail();
         $nostoCustomer = new Nosto_Object_Customer();
+        /** @noinspection PhpUndefinedMethodInspection */
         $nostoCustomer->setFirstName($customer->getFirstname());
+        /** @noinspection PhpUndefinedMethodInspection */
         $nostoCustomer->setLastName($customer->getLastname());
         $nostoCustomer->setCustomerReference($this->getCustomerReference());
         $nostoCustomer->setEmail($email);
