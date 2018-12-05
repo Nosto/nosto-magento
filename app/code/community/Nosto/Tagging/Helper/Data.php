9255df50470cfd455911dd0c75c1e0014813152c
Cid Lopes <alannettto@gmail.com>
2018-12-05 15:32:40 +0200

    Add Feature Flag For Sending Inventory Level After Purchase

--- app/code/community/Nosto/Tagging/Helper/Data.php
+++ app/code/community/Nosto/Tagging/Helper/Data.php
@@ -136,6 +136,11 @@ class Nosto_Tagging_Helper_Data extends Mage_Core_Helper_Abstract
     const XML_PATH_USE_INVENTORY_LEVEL = 'nosto_tagging/general/use_inventory_level';
 
     /**
+     * Path to store config for sending inventory level after purchase
+     */
+    const XML_PATH_SEND_INVENTORY_LEVEL_AFTER_PURCHASE = 'nosto_tagging/general/send_inventory_level_after_purchase';
+
+    /**
      * Path to store config for tagging low stock
      */
     const XML_PATH_USE_LOW_STOCK = 'nosto_tagging/general/use_low_stock';
@@ -575,6 +580,17 @@ class Nosto_Tagging_Helper_Data extends Mage_Core_Helper_Abstract
     public function getUseInventoryLevel($store = null)
     {
         return (bool)Mage::getStoreConfig(self::XML_PATH_USE_INVENTORY_LEVEL, $store);
+    }
+
+    /**
+     * Returns on/off setting for sending inventory level after purchase
+     *
+     * @param Mage_Core_Model_Store|null $store the store model or null.
+     * @return boolean
+     */
+    public function getSendInventoryLevelAfterPurchase($store = null)
+    {
+        return (bool)Mage::getStoreConfig(self::XML_PATH_SEND_INVENTORY_LEVEL_AFTER_PURCHASE, $store);
     }
 
     /**
