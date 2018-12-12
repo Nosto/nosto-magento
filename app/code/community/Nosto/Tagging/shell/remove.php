4d6cfdfd34c987b254e58aa94086fe1164df593f
Olsi Qose <olsi.qose@nosto.com>
2018-12-12 15:55:49 +0200

    Flush cache after account removal

--- app/code/community/Nosto/Tagging/shell/remove.php
+++ app/code/community/Nosto/Tagging/shell/remove.php
@@ -77,7 +77,7 @@ class RemoveCommand extends Mage_Shell_Abstract
             exit(1);
         }
         if ($this->_accountHelper->resetAccountSettings($store)) {
-            Mage::app()->getCacheInstance()->cleanType('config');
+            Mage::app()->getCacheInstance()->flush();
             echo "Account removed from store. \n";
         } else {
             echo sprintf("Operation failed");
