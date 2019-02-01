b88f63546fcb29523e17d6b7053dde85a3d1e7d5
Hannu Pölönen <hannu.polonen@nosto.com>
2019-02-01 15:33:51 +0200

    Catch Zend_Db_Stamement_Exception that happens occasionally in Nosto customer update

--- app/code/community/Nosto/Tagging/Helper/Customer.php
+++ app/code/community/Nosto/Tagging/Helper/Customer.php
@@ -82,14 +82,18 @@ class Nosto_Tagging_Helper_Customer extends Mage_Core_Helper_Abstract
             /** @noinspection PhpUndefinedMethodInspection */
             if ($customer->hasData()) {
                 $customer->setUpdatedAt($dateHelper->gmtDate());
-                $customer->save();
             } else {
                 $restoreCartHash = $this->generateRestoreCartHash();
                 $customer->setQuoteId($quoteId);
                 $customer->setNostoId($nostoId);
                 $customer->setRestoreCartHash($restoreCartHash);
                 $customer->setCreatedAt($dateHelper->gmtDate());
+            }
+            try {
                 $customer->save();
+            } catch (Zend_Db_Statement_Exception $e) {
+                // We don't need to care about the duplicate key exception
+                // that might happend occasionally due to concurrency
             }
         }
     }
