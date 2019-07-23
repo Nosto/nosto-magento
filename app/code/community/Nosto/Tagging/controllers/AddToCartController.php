a85af62f37d3d5b38f8b643eeaccee40d5455a51
Cid Lopes <alannettto@gmail.com>
2019-07-23 11:18:21 +0300

    Extract getOptionAttributes into method

--- app/code/community/Nosto/Tagging/controllers/AddToCartController.php
+++ app/code/community/Nosto/Tagging/controllers/AddToCartController.php
@@ -69,21 +69,7 @@ class Nosto_Tagging_AddToCartController extends Mage_Checkout_CartController
             $parentType = $product->getTypeInstance();
             $attributeOptions = array();
             if ($parentType instanceof Mage_Catalog_Model_Product_Type_Configurable) {
-                $skuProduct = Mage::getModel('catalog/product')->load($skuId);
-                $configurableAttributes = $parentType->getConfigurableAttributesAsArray($product);
-                foreach ($configurableAttributes as $configurableAttribute) {
-                    $attributeCode = $configurableAttribute['attribute_code'];
-                    /** @var \Mage_Catalog_Model_Resource_Product $productResource */
-                    $productResource = $skuProduct->getResource();
-                    $attribute = $productResource->getAttribute($attributeCode);
-                    if ($attribute instanceof Mage_Catalog_Model_Resource_Eav_Attribute) {
-                        $attributeId = $attribute->getId();
-                        $attributeValueId = $skuProduct->getData($attributeCode);
-                        if ($attributeId && $attributeValueId) {
-                            $attributeOptions[$attributeId] = $attributeValueId;
-                        }
-                    }
-                }
+                $attributeOptions = $this->getOptionAttributes($skuId, $parentType, $product);
             }
 
             if (empty($attributeOptions)) {
@@ -137,99 +123,87 @@ class Nosto_Tagging_AddToCartController extends Mage_Checkout_CartController
             /** @noinspection PhpUnhandledExceptionInspection */
             Mage::throwException('Invalid form key');
         }
-
         $cart = $this->_getCart();
-
         $products = explode(',', $this->getRequest()->getParam('product'));
         $skus = explode(',', $this->getRequest()->getParam('skus'));
-
         foreach ($products as $key => $product) {
             try {
                 /* @var Mage_Catalog_Model_Product $product */
-                $product = $this->initProductById($product);
+                $this->getRequest()->setParam('product', $product);
+                $product = $this->_initProduct();
                 if (!$product) {
                     return $this->_goBack();
                 }
-                /** @var Mage_Catalog_Model_Product_Type_Configurable $parentType */
                 $parentType = $product->getTypeInstance();
-                // We need to get the SKU parent, then load the SKU again to get the super attributes
-                $attributeOptions = array();
+                $params = null;
                 if ($parentType instanceof Mage_Catalog_Model_Product_Type_Configurable) {
-                    $skuProduct = Mage::getModel('catalog/product')->load($skus[$key]);
-                    $configurableAttributes = $parentType->getConfigurableAttributesAsArray($product);
-                    foreach ($configurableAttributes as $configurableAttribute) {
-                        $attributeCode = $configurableAttribute['attribute_code'];
-                        /** @var \Mage_Catalog_Model_Resource_Product $productResource */
-                        $productResource = $skuProduct->getResource();
-                        $attribute = $productResource->getAttribute($attributeCode);
-                        if ($attribute instanceof Mage_Catalog_Model_Resource_Eav_Attribute) {
-                            $attributeId = $attribute->getId();
-                            $attributeValueId = $skuProduct->getData($attributeCode);
-                            if ($attributeId && $attributeValueId) {
-                                $attributeOptions[$attributeId] = $attributeValueId;
-                            }
-                        }
-                    }
+                    $attributeOptions = $this->getOptionAttributes($skus[$key], $parentType, $product);
                     if (empty($attributeOptions)) {
-                        $this->_getSession()->addError($this->__('Cannot add the item to shopping cart.'));
+                        $this->_getSession()->addError(
+                            $this->__(
+                                'Cannot add %s to shopping cart.',
+                                Mage::helper('core')->escapeHtml($product->getName())
+                            )
+                        );
                         return $this->_goBack();
                     }
                     $params = array('super_attribute' => $attributeOptions);
-                } elseif ($parentType instanceof Mage_Catalog_Model_Product_Type_Simple){
-
                 }
-
-                $qty = $this->getRequest()->getParam('qty');
-                if ($qty !== null) {
-                    $params['qty'] = $qty;
-                }
-                /* Below is cannibalized from parent */
                 $cart->addProduct($product, $params);
-
                 /** @noinspection PhpUndefinedMethodInspection */
-                if (!$this->_getSession()->getNoCartRedirect(true)) {
-                    /** @noinspection PhpUndefinedMethodInspection */
-                    if (!$cart->getQuote()->getHasError()) {
-                        $message = $this->__(
-                            '%s was added to your shopping cart.',
-                            Mage::helper('core')->escapeHtml($product->getName())
-                        );
-                        $this->_getSession()->addSuccess($message);
-                    }
-
+                if (!$this->_getSession()->getNoCartRedirect(true) && !$cart->getQuote()->getHasError()) {
+                    $message = $this->__(
+                        '%s was added to your shopping cart.',
+                        Mage::helper('core')->escapeHtml($product->getName())
+                    );
+                    $this->_getSession()->addSuccess($message);
                 }
+                Mage::dispatchEvent(
+                    'checkout_cart_add_product_complete',
+                    array('product' => $product, 'request' => $this->getRequest(), 'response' => $this->getResponse())
+                );
             } catch (\Exception $e) {
                 $this->_getSession()->addException($e, $this->__('Cannot add the item to shopping cart.'));
                 Mage::logException($e);
+                /** @noinspection PhpUndefinedMethodInspection */
+                $this->_getSession()->setCartWasUpdated(false);
+                return $this->_goBack();
             }
         }
         $cart->save();
         /** @noinspection PhpUndefinedMethodInspection */
         $this->_getSession()->setCartWasUpdated(true);
-        Mage::dispatchEvent(
-            'checkout_cart_add_product_complete',
-            array('product' => $product, 'request' => $this->getRequest(), 'response' => $this->getResponse())
-        );
         return $this->_goBack();
     }
 
     /**
-     * Initialize product instance from request data
-     *
-     * @param $productId
-     * @return bool|Mage_Catalog_Model_Product
-     * @throws Mage_Core_Model_Store_Exception
+     * @param $skuId
+     * @param Mage_Catalog_Model_Product_Type_Configurable $parentType
+     * @param Mage_Catalog_Model_Product $product
+     * @return array
      */
-    protected function initProductById($productId)
-    {
-        /* @var Mage_Catalog_Model_Product $product */
-        $product = Mage::getModel('catalog/product')
-            ->setStoreId(Mage::app()->getStore()->getId())
-            ->load($productId);
-        if ($product->getId()) {
-            return $product;
+    private function getOptionAttributes(
+        $skuId,
+        Mage_Catalog_Model_Product_Type_Configurable $parentType,
+        Mage_Catalog_Model_Product $product
+    ) {
+        $attributeOptions = array();
+        $skuProduct = Mage::getModel('catalog/product')->load($skuId);
+        $configurableAttributes = $parentType->getConfigurableAttributesAsArray($product);
+        foreach ($configurableAttributes as $configurableAttribute) {
+            $attributeCode = $configurableAttribute['attribute_code'];
+            /** @var \Mage_Catalog_Model_Resource_Product $productResource */
+            $productResource = $skuProduct->getResource();
+            $attribute = $productResource->getAttribute($attributeCode);
+            if ($attribute instanceof Mage_Catalog_Model_Resource_Eav_Attribute) {
+                $attributeId = $attribute->getId();
+                $attributeValueId = $skuProduct->getData($attributeCode);
+                if ($attributeId && $attributeValueId) {
+                    $attributeOptions[$attributeId] = $attributeValueId;
+                }
+            }
         }
-        return false;
+        return $attributeOptions;
     }
 
 }
