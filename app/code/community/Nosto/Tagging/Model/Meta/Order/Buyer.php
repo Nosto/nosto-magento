f239ccc2af5416414f57d8b9bb2a00a71d84a590
Cid Lopes <alannettto@gmail.com>
2022-03-08 15:30:45 +0200

    Remove PII data

--- app/code/community/Nosto/Tagging/Model/Meta/Order/Buyer.php
+++ app/code/community/Nosto/Tagging/Model/Meta/Order/Buyer.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Magento
- *  
+ *
  * NOTICE OF LICENSE
- *  
+ *
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
@@ -11,13 +11,13 @@
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to license@magentocommerce.com so we can send you a copy immediately.
- *  
+ *
  * DISCLAIMER
- *  
+ *
  * Do not edit or add to this file if you wish to upgrade Magento to newer
  * versions in the future. If you wish to customize Magento for your
  * needs please refer to http://www.magentocommerce.com for more information.
- *  
+ *
  * @category  Nosto
  * @package   Nosto_Tagging
  * @author    Nosto Solutions Ltd <magento@nosto.com>
@@ -53,17 +53,12 @@ class Nosto_Tagging_Model_Meta_Order_Buyer extends Nosto_Object_Order_Buyer
             return false;
         }
 
-        $this->setFirstName($order->getCustomerFirstname());
-        $this->setLastName($order->getCustomerLastname());
-        $this->setEmail($order->getCustomerEmail());
         /** @var Nosto_Tagging_Helper_Email $emailHelper */
         $emailHelper = Mage::helper('nosto_tagging/email');
         $optedIn = $emailHelper->isOptedIn($order->getCustomerEmail());
         $this->setMarketingPermission($optedIn);
         $address = $order->getBillingAddress();
         if ($address instanceof Mage_Sales_Model_Order_Address) {
-            $this->setPhone($address->getTelephone());
-            $this->setPostCode($address->getPostcode());
             $this->setCountry($address->getCountry());
         }
 
